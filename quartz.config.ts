51dbcdd0da4c5733387cced488fba262bf6be14a
ern-sys <ern-syscode@gmail.com>
2026-04-15 09:14:59 +0700

    Updated

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://ern-sys.github.io/",
+    baseUrl: "https://github.com/ern-sys/ern-sys.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
